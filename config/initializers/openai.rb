1a31a3a48e4d49a0e310e597b70bd6725e45c266
davezen316 <davezen316@gmail.com>
2023-11-01 16:43:05 +0800

    modified openai config

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,9 @@
+# OpenAI.configure do |config|
+#   config.access_token =  Rails.application.credentials.openai.access_token
+#   config.organization_id = Rails.application.credentials.openai.organization_id
+# end
+
 OpenAI.configure do |config|
-  config.access_token =  Rails.application.credentials.openai.access_token
-  config.organization_id = Rails.application.credentials.openai.organization_id
+  config.access_token = ENV['OPENAI_ACCESS_TOKEN']
+  config.organization_id = "org-Og9DHM7SL0t4JJmsZ3Pe47fT"
 end
